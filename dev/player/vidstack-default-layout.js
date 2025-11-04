b5fd9b6e4fb90c046923683bd73057fdcd1f8347
Jury Giannelli <jury.giannelli@sdox.io>
2025-11-04 14:33:54 +0100

    build

---
+++ dev/player/vidstack-default-layout.js
@@ -0,0 +1,14 @@
+"use client"
+
+export { DefaultAudioLayout, DefaultBufferingIndicator, DefaultKeyboardDisplay, DefaultLayoutContext, DefaultMenuButton, DefaultMenuCheckbox, DefaultMenuItem, DefaultMenuRadioGroup, DefaultMenuSection, DefaultMenuSliderItem, DefaultSliderParts, DefaultSliderSteps, DefaultTooltip, DefaultVideoGestures, DefaultVideoLargeLayout, DefaultVideoLayout, DefaultVideoSmallLayout, createRadioOptions, i18n, useDefaultLayoutContext, useDefaultLayoutWord } from '../chunks/vidstack-EMJ6NoyJ.js';
+export { defaultLayoutIcons } from './vidstack-default-icons.js';
+import 'react';
+import '../chunks/vidstack-D_bWd66h.js';
+import '../chunks/vidstack-D2S8_iz5.js';
+import '../chunks/vidstack-Dk5mBh_U.js';
+import '@floating-ui/dom';
+import '../chunks/vidstack-CmiGbgCP.js';
+import 'react-dom';
+import '../chunks/vidstack-DawPldi7.js';
+import '../chunks/vidstack-D-hQD1eE.js';
+import '../chunks/vidstack-CBF7iUqu.js';
---
+++ prod/player/vidstack-default-layout.js
@@ -0,0 +1,14 @@
+"use client"
+
+export { DefaultAudioLayout, DefaultBufferingIndicator, DefaultKeyboardDisplay, DefaultLayoutContext, DefaultMenuButton, DefaultMenuCheckbox, DefaultMenuItem, DefaultMenuRadioGroup, DefaultMenuSection, DefaultMenuSliderItem, DefaultSliderParts, DefaultSliderSteps, DefaultTooltip, DefaultVideoGestures, DefaultVideoLargeLayout, DefaultVideoLayout, DefaultVideoSmallLayout, createRadioOptions, i18n, useDefaultLayoutContext, useDefaultLayoutWord } from '../chunks/vidstack-BuBfsk3D.js';
+export { defaultLayoutIcons } from './vidstack-default-icons.js';
+import 'react';
+import '../chunks/vidstack-CZgUA94N.js';
+import '../chunks/vidstack-BJSIP8ZT.js';
+import '../chunks/vidstack-Dd9F2fTb.js';
+import '@floating-ui/dom';
+import '../chunks/vidstack-DLhL2w4R.js';
+import 'react-dom';
+import '../chunks/vidstack-C8ugc8lW.js';
+import '../chunks/vidstack-vqp2QaHR.js';
+import '../chunks/vidstack-CBF7iUqu.js';
